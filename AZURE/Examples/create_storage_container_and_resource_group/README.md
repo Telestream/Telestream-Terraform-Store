677a726f6e134dea7d88828f40f8697680ec5374
kevin-travers-telestream <129967725+kevin-travers-telestream@users.noreply.github.com>
2023-04-07 08:53:00 -0400

    Created Examples for Azure bucket module

---
+++ AZURE/Examples/create_storage_container_and_resource_group/README.md
@@ -0,0 +1,32 @@
+## Requirements
+
+No requirements.
+
+## Providers
+
+No providers.
+
+## Modules
+
+| Name | Source | Version |
+|------|--------|---------|
+| <a name="module_bucket"></a> [bucket](#module\_bucket) | ../../Bucket | n/a |
+
+## Resources
+
+No resources.
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| <a name="output_bucket_names"></a> [bucket\_names](#output\_bucket\_names) | List of bucket names created |
+| <a name="output_storage_account_name"></a> [storage\_account\_name](#output\_storage\_account\_name) | name of the storage account |
+| <a name="output_storage_account_primary_access_key"></a> [storage\_account\_primary\_access\_key](#output\_storage\_account\_primary\_access\_key) | The primary access key for the storage account. |
+| <a name="output_storage_account_primary_connection_string"></a> [storage\_account\_primary\_connection\_string](#output\_storage\_account\_primary\_connection\_string) | The connection string associated with the primary location. |
+| <a name="output_storage_account_secondary_access_key"></a> [storage\_account\_secondary\_access\_key](#output\_storage\_account\_secondary\_access\_key) | The secondary access key for the storage account. |
+| <a name="output_storage_account_secondary_connection_string"></a> [storage\_account\_secondary\_connection\_string](#output\_storage\_account\_secondary\_connection\_string) | The connection string associated with the secondary location. |
---
+++ AZURE/Examples/create_storage_container_and_resource_group/main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  # Configuration options
+  features {
+  }
+}
+
+module "bucket" {
+  source       = "../../Bucket"
+  bucket_names = ["<replace_with_unique_name_of_bucket>"]
+  azurerm_resource_group = {
+    name     = "<replace_with_unique_name_for_azurerm_resource_group>"
+    location = "<replace_with_unique_name_azure_location"
+  }
+  storage_account = {
+    name = "<replace_with_unique_name_for_storeage_account>"
+  }
+}
+
+output "bucket_names" {
+  value       = module.bucket.storage_container_name
+  description = "List of bucket names created"
+}
+output "storage_account_name" {
+  value       = module.bucket.storage_account_name
+  description = "name of the storage account"
+}
+output "storage_account_primary_access_key" {
+  description = "The primary access key for the storage account."
+  value       = module.bucket.storage_account_primary_access_key
+  sensitive   = true
+}
+output "storage_account_primary_connection_string" {
+  description = "The connection string associated with the primary location."
+  value       = module.bucket.storage_account_primary_connection_string
+  sensitive   = true
+}
+output "storage_account_secondary_access_key" {
+  description = "The secondary access key for the storage account."
+  value       = module.bucket.storage_account_secondary_access_key
+  sensitive   = true
+}
+output "storage_account_secondary_connection_string" {
+  description = "The connection string associated with the secondary location."
+  value       = module.bucket.storage_account_secondary_connection_string
+  sensitive   = true
+}
